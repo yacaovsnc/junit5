716bf9c143520d655a9f43e925d073bd62e3b42d
Sam Brannen <sam@sambrannen.com>
2018-08-06 15:54:28 +0200

    Example suites for #1520

---
+++ platform-tests/src/test/java/org/example/TestSuite3.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2015-2018 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
+ *
+ * http://www.eclipse.org/legal/epl-v20.html
+ */
+
+package org.example;
+
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.runner.RunWith;
+
+/**
+ * <h3>Logging Configuration</h3>
+ *
+ * <p>In order for our log4j2 configuration to be used in an IDE, you must
+ * set the following system property before running any tests &mdash; for
+ * example, in <em>Run Configurations</em> in Eclipse.
+ *
+ * <pre class="code">
+ * -Djava.util.logging.manager=org.apache.logging.log4j.jul.LogManager
+ * </pre>
+ */
+@RunWith(JUnitPlatform.class)
+@SelectClasses(TestSuite2.class)
+class TestSuite3 {
+}
