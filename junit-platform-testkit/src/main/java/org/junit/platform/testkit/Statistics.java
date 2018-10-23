108f862cd9e095309d98360b1165947935b77263
Sam Brannen <sam@sambrannen.com>
2018-10-23 17:05:18 +0200

    Ensure Test Kit compiles on JDK 8

--- junit-platform-testkit/src/main/java/org/junit/platform/testkit/Statistics.java
+++ junit-platform-testkit/src/main/java/org/junit/platform/testkit/Statistics.java
@@ -12,9 +12,6 @@ package org.junit.platform.testkit;
 
 import static org.apiguardian.api.API.Status.EXPERIMENTAL;
 
-import java.util.function.Supplier;
-import java.util.stream.Stream;
-
 import org.apiguardian.api.API;
 
 /**
@@ -27,31 +24,27 @@ public interface Statistics<T> {
 	void assertStatistic(T results);
 
 	public static Statistics<TestResults> skipped(long expected) {
-		return results -> assertStatistic(expected, results::skipped, "skipped");
+		return results -> StatisticsUtils.assertStatistic(expected, results::skipped, "skipped");
 	}
 
 	public static Statistics<TestResults> started(long expected) {
-		return results -> assertStatistic(expected, results::started, "started");
+		return results -> StatisticsUtils.assertStatistic(expected, results::started, "started");
 	}
 
 	public static Statistics<TestResults> finished(long expected) {
-		return results -> assertStatistic(expected, results::finished, "finished");
+		return results -> StatisticsUtils.assertStatistic(expected, results::finished, "finished");
 	}
 
 	public static Statistics<TestResults> aborted(long expected) {
-		return results -> assertStatistic(expected, results::aborted, "aborted");
+		return results -> StatisticsUtils.assertStatistic(expected, results::aborted, "aborted");
 	}
 
 	public static Statistics<TestResults> succeeded(long expected) {
-		return results -> assertStatistic(expected, results::succeeded, "succeeded");
+		return results -> StatisticsUtils.assertStatistic(expected, results::succeeded, "succeeded");
 	}
 
 	public static Statistics<TestResults> failed(long expected) {
-		return results -> assertStatistic(expected, results::failed, "failed");
-	}
-
-	private static void assertStatistic(long expected, Supplier<Stream<?>> streamSupplier, String category) {
-		Assertions.assertEquals(expected, streamSupplier.get().count(), category);
+		return results -> StatisticsUtils.assertStatistic(expected, results::failed, "failed");
 	}
 
 }
